44e6b647000e650f27048c77ae836f7cb2ef8107
MarianoJPerez <marianperez77@outlook.com>
2024-11-11 18:34:01 -0300

    colores para botones y estetica de la gamelist

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,17 @@ import React from 'react';
 
 const Navbar = ({ currentView, setCurrentView }) => {
   return (
-    <nav>
+    <nav className="bg-red-600 p-1">
       <ul>
-        <li onClick={() => setCurrentView('games')} style={{ cursor: 'pointer' }}>
+        <li onClick={() => setCurrentView('games')} className="cursor-pointer text-white bg-black font-bold text-lg px-4 py-2 rounded border-2  inline-block hover:bg-blue-600">
           Lista de Juegos
         </li>
-        <li onClick={() => setCurrentView('wishlist')} style={{ cursor: 'pointer' }}>
-          Lista de Deseados
-        </li>
+        <li
+  onClick={() => setCurrentView('wishlist')}
+  className="cursor-pointer text-white bg-black font-bold text-lg px-4 py-2 rounded border-2  inline-block hover:bg-blue-600"
+>
+  Lista de Deseados
+</li>
       </ul>
     </nav>
   );
