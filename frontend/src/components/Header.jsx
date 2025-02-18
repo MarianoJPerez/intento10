698d6ccdcf927817d1d7f44ea412fbec92c329d2
MarianoJPerez <marianperez77@outlook.com>
2025-02-18 19:45:53 -0300

    Me olvide de guardar el header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import logo from '../assets/logo.jpg';
-import '../assets/Header.css'; // Importa el CSS
+import '../assets/Header.css'; 
 
 const Header = ({ currentUser, handleLogout }) => {
   return (
     <header className="header-container">
-      {/* Sección Logo + Título */}
       <div className="header-logo-container">
         <img src={logo} alt="Logo de la tienda" className="header-logo" />
         <h1 className="header-title">INFINITY GAMES</h1>
       </div>
 
-      {/* Sección Usuario + Botón de Logout */}
       <div className="header-user-container">
         <p className="header-welcome-text">
           Bienvenido, <span className="username">{currentUser.username}</span>
